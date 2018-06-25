97f2f913403c0d07389bc9eb4169038500643a5d
github username <github email address>
2018-06-25 13:44:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (driversArray) {
+  return driversArray.slice(0,2);
+};
+
+const returnLastTwoDrivers = function (driversArray) {
+  return driversArray.slice(-2);
+};
+
+selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//This will output a function. NO VALUE. That function will be used later on.
+
+//literally just making a function here!!!!!!!
+const createFareMultiplier = function (multiplier) {
+  return function (price) {
+    return price * multiplier;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function ( driversArray, specifiedDriverFunction ) {
+    return specifiedDriverFunction(driversArray);
+    //return something
+  };
